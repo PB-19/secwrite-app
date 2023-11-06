6e08cb9154de41975fab28d9f503e607ca4983d5
Pranav Balaji <pb19.official@gmail.com>
2023-11-06 15:12:02 +0530

    initial commit

--- lib/src/utils/constants/typography.dart
+++ lib/src/utils/constants/typography.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:secwriteapp/src/constants/colors.dart';
+import 'colors.dart';
 
 class ApplicationTextTheme {
   static TextTheme lightTextTheme = TextTheme(
