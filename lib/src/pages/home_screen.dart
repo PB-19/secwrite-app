19e96ad112ea5e1bae745b8a3533884bcdf4e96d
Pranav Balaji <pb19.official@gmail.com>
2023-11-07 20:25:18 +0530

    home screen set

--- lib/src/pages/home_screen.dart
+++ lib/src/pages/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Welcome!"),
+      body: Center(child: Text("Welcome!"),),
     );
   }
 }
