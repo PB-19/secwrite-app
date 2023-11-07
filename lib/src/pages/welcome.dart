d845e00a7e3075d5ec7686910b1a6244bc24e14d
Pranav Balaji <pb19.official@gmail.com>
2023-11-07 12:03:08 +0530

    welcome screen

--- lib/src/pages/welcome.dart
+++ lib/src/pages/welcome.dart
@@ -6,12 +6,48 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 50,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image(image: AssetImage(welcomeImage)),
+            Image.asset(welcomeImage),
+            Column(
+              children: [
+                Text(
+                  "Welcome to SecWrite!",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20,),
+                Text(
+                  "Your private note-keeping app!",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text("LOGIN"),
+                  ),
+                ),
+                const SizedBox(width: 10,),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text("REGISTER"),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
--- lib/src/utils/constants/typography.dart
+++ lib/src/utils/constants/typography.dart
@@ -5,155 +5,95 @@ import 'colors.dart';
 class ApplicationTextTheme {
   static TextTheme lightTextTheme = TextTheme(
     displayLarge: GoogleFonts.montserrat(
-      fontSize: 57,
-      height: 64,
       color: LightColorPalette.onSurface,
     ),
     displayMedium: GoogleFonts.montserrat(
-      fontSize: 45,
-      height: 52,
       color: LightColorPalette.onSurface,
     ),
     displaySmall: GoogleFonts.montserrat(
-      fontSize: 36,
-      height: 44,
       color: LightColorPalette.onSurface,
     ),
     headlineLarge: GoogleFonts.montserrat(
-      fontSize: 32,
-      height: 40,
       color: LightColorPalette.onSurface,
     ),
     headlineMedium: GoogleFonts.montserrat(
-      fontSize: 28,
-      height: 36,
       color: LightColorPalette.onSurface,
     ),
     headlineSmall: GoogleFonts.montserrat(
-      fontSize: 24,
-      height: 32,
       color: LightColorPalette.onSurface,
     ),
     titleLarge: GoogleFonts.montserrat(
-      fontSize: 22,
-      height: 28,
       color: LightColorPalette.onSurface,
     ),
     titleMedium: GoogleFonts.montserrat(
-      fontSize: 16,
-      height: 24,
       color: LightColorPalette.onSurface,
     ),
     titleSmall: GoogleFonts.montserrat(
-      fontSize: 14,
-      height: 20,
       color: LightColorPalette.onSurface,
     ),
     bodyLarge: GoogleFonts.poppins(
-      fontSize: 16,
-      height: 24,
       color: LightColorPalette.onSurfaceVariant,
     ),
     bodyMedium: GoogleFonts.poppins(
-      fontSize: 14,
-      height: 20,
       color: LightColorPalette.onSurfaceVariant,
     ),
     bodySmall: GoogleFonts.poppins(
-      fontSize: 12,
-      height: 16,
       color: LightColorPalette.onSurfaceVariant,
     ),
     labelLarge: GoogleFonts.poppins(
-      fontSize: 14,
-      height: 20,
       color: LightColorPalette.onSurfaceVariant,
     ),
     labelMedium: GoogleFonts.poppins(
-      fontSize: 12,
-      height: 16,
       color: LightColorPalette.onSurfaceVariant,
     ),
     labelSmall: GoogleFonts.poppins(
-      fontSize: 11,
-      height: 16,
       color: LightColorPalette.onSurfaceVariant,
     ),
   );
   static TextTheme darkTextTheme = TextTheme(
     displayLarge: GoogleFonts.montserrat(
-      fontSize: 57,
-      height: 64,
       color: DarkColorPalette.onSurface,
     ),
     displayMedium: GoogleFonts.montserrat(
-      fontSize: 45,
-      height: 52,
       color: DarkColorPalette.onSurface,
     ),
     displaySmall: GoogleFonts.montserrat(
-      fontSize: 36,
-      height: 44,
       color: DarkColorPalette.onSurface,
     ),
     headlineLarge: GoogleFonts.montserrat(
-      fontSize: 32,
-      height: 40,
       color: DarkColorPalette.onSurface,
     ),
     headlineMedium: GoogleFonts.montserrat(
-      fontSize: 28,
-      height: 36,
       color: DarkColorPalette.onSurface,
     ),
     headlineSmall: GoogleFonts.montserrat(
-      fontSize: 24,
-      height: 32,
       color: DarkColorPalette.onSurface,
     ),
     titleLarge: GoogleFonts.montserrat(
-      fontSize: 22,
-      height: 28,
       color: DarkColorPalette.onSurface,
     ),
     titleMedium: GoogleFonts.montserrat(
-      fontSize: 16,
-      height: 24,
       color: DarkColorPalette.onSurface,
     ),
     titleSmall: GoogleFonts.montserrat(
-      fontSize: 14,
-      height: 20,
       color: DarkColorPalette.onSurface,
     ),
     bodyLarge: GoogleFonts.poppins(
-      fontSize: 16,
-      height: 24,
       color: DarkColorPalette.onSurfaceVariant,
     ),
     bodyMedium: GoogleFonts.poppins(
-      fontSize: 14,
-      height: 20,
       color: DarkColorPalette.onSurfaceVariant,
     ),
     bodySmall: GoogleFonts.poppins(
-      fontSize: 12,
-      height: 16,
       color: DarkColorPalette.onSurfaceVariant,
     ),
     labelLarge: GoogleFonts.poppins(
-      fontSize: 14,
-      height: 20,
       color: DarkColorPalette.onSurfaceVariant,
     ),
     labelMedium: GoogleFonts.poppins(
-      fontSize: 12,
-      height: 16,
       color: DarkColorPalette.onSurfaceVariant,
     ),
     labelSmall: GoogleFonts.poppins(
-      fontSize: 11,
-      height: 16,
       color: DarkColorPalette.onSurfaceVariant,
     ),
   );
