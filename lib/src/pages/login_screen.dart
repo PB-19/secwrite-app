f73257a4706f414bbfaa80f4abc76ac0cf186328
Pranav Balaji <pb19.official@gmail.com>
2023-11-07 20:16:38 +0530

    login screen

--- lib/src/pages/login_screen.dart
+++ lib/src/pages/login_screen.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:secwriteapp/src/utils/constants/strings.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Login Screen"),),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+          top: 50,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(welcomeImage),
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20.0,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome back!",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0,),
+            const Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: UnderlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10.0,),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: UnderlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  autocorrect: false,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0,),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: FilledButton(
+                        onPressed: () {
+                          
+                        },
+                        child: const Text("LOGIN"),
+                      ),
+                    ),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/register/');
+                  },
+                  child: const Text("Don't have an account? Register here!"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
